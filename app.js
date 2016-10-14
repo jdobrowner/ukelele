d7f822591771adabd663ee8728706072e3e5b9fc
jdobrowner <j@jasondobrowner.com>
2016-10-14 08:25:31 -0700

    using wav files

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ $(function() {
 
 function makeSounds() {
 
-  var multiPlayer = new Tone.MultiPlayer(["ukeTones/A.mp3", "ukeTones/C#.mp3"],
-function(){
-	//if an array is passed in, the samples are referenced to by index
-
-});
+//   var multiPlayer = new Tone.MultiPlayer(["ukeTones/A.wav", "ukeTones/E.wav"],
+// function(){
+// 	//if an array is passed in, the samples are referenced to by index
+//   //multiPlayer.start(1);
+// }).toMaster();
 
 
 
     $('button').click( function(e) {
       console.log('click');
+      var multiPlayer = new Tone.MultiPlayer(["ukeTones/A.wav", "ukeTones/E.wav"],
+    function(){
+    	//if an array is passed in, the samples are referenced to by index
       multiPlayer.start(1);
+      console.log('sound');
+    }).toMaster();
     });
 };
